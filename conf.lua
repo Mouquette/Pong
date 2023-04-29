fb15d6370c588efc5f94032a60f31befaa9c1578
Virgil Mendes Dos Santos <virgil.mendesdossantos@vivaticket.com>
2023-04-29 21:47:19 +0200

    No Fullscreen
    - Remove Fullscreen mode
    - Set game in 1024x768

--- conf.lua
+++ conf.lua
@@ -2,17 +2,10 @@ local DEBUG = false
 
 function love.conf(t)
   t.window.title = "Pong"
+  t.window.width = 1024
+  t.window.height = 768
 
-  if DEBUG then
-    t.console = true
-    t.window.width = 1024
-    t.window.height = 768
-  else
-    t.console = false
-    t.window.width = 1920
-    t.window.height = 1080
-    t.window.fullscreen = true
-  end
+  t.console = DEBUG
 
   t.modules.data = false
   t.modules.joystick = false
